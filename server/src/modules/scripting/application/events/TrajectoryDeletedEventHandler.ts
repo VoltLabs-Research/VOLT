e7d8ca55272dd108b552f1cff99346a335e345c3
rodyherrera <contact@rodyherrera.com>
2026-02-25 01:15:04 -0300

    feat: delete associated Jupyter sessions when a trajectory is removed

--- server/src/modules/scripting/application/events/TrajectoryDeletedEventHandler.ts
+++ server/src/modules/scripting/application/events/TrajectoryDeletedEventHandler.ts
@@ -4,15 +4,21 @@ import TrajectoryDeletedEvent from '@modules/trajectory/domain/events/Trajectory
 import { SCRIPTING_TOKENS } from '@modules/scripting/infrastructure/di/ScriptingTokens';
 import type { IScriptingNotebookRepository } from '@modules/scripting/domain/ports/IScriptingNotebookRepository';
 
+import { JupyterService } from '@modules/scripting/infrastructure/services/JupyterService';
+
 @injectable()
 export default class TrajectoryDeletedEventHandler implements IEventHandler<TrajectoryDeletedEvent> {
     constructor(
         @inject(SCRIPTING_TOKENS.ScriptingNotebookRepository)
-        private readonly scriptingNotebookRepository: IScriptingNotebookRepository
+        private readonly scriptingNotebookRepository: IScriptingNotebookRepository,
+
+        @inject(JupyterService)
+        private readonly jupyterService: JupyterService
     ){}
 
     async handle(event: TrajectoryDeletedEvent): Promise<void> {
         const { trajectoryId } = event.payload;
         await this.scriptingNotebookRepository.removeTrajectory(trajectoryId);
+        await this.jupyterService.deleteSession(trajectoryId);
     }
 }
