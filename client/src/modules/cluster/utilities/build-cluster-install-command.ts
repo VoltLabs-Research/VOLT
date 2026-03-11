bd25e0949f74e90055293be530c63f40dc3d9c79
rodyherrera <contact@rodyherrera.com>
2026-03-10 22:21:46 -0300

    refactor: update cluster installation command and remove obsolete setup script

--- client/src/modules/cluster/utilities/build-cluster-install-command.ts
+++ client/src/modules/cluster/utilities/build-cluster-install-command.ts
@@ -1,5 +1,5 @@
 export const buildClusterInstallCommand = (teamClusterId: string, enrollmentToken: string): string => {
     const cloudUrl = import.meta.env.VITE_API_URL;
 
-    return `curl -sSL https://raw.githubusercontent.com/voltlabs-research/volt/main/tools/setup-cluster.sh | VOLT_CLOUD_URL="${cloudUrl}" bash -s -- "${teamClusterId}" "${enrollmentToken}"`;
+    return `curl -sSL https://raw.githubusercontent.com/voltlabs-research/clusterdaemon/main/tools/install.sh | VOLT_CLOUD_URL="${cloudUrl}" bash -s -- "${teamClusterId}" "${enrollmentToken}"`;
 };
