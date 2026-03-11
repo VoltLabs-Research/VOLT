9eb729f58976386d1b4ad5fd8d8502635e030ba6
rodyherrera <contact@rodyherrera.com>
2026-03-10 23:49:22 -0300

    fix: update install script path in buildClusterInstallCommand function

--- client/src/modules/cluster/utilities/build-cluster-install-command.ts
+++ client/src/modules/cluster/utilities/build-cluster-install-command.ts
@@ -1,5 +1,5 @@
 export const buildClusterInstallCommand = (teamClusterId: string, enrollmentToken: string): string => {
     const cloudUrl = import.meta.env.VITE_API_URL;
 
-    return `curl -sSL https://raw.githubusercontent.com/voltlabs-research/clusterdaemon/main/tools/install.sh | VOLT_CLOUD_URL="${cloudUrl}" bash -s -- "${teamClusterId}" "${enrollmentToken}"`;
+    return `curl -sSL https://raw.githubusercontent.com/voltlabs-research/clusterdaemon/main/scripts/install.sh | VOLT_CLOUD_URL="${cloudUrl}" bash -s -- "${teamClusterId}" "${enrollmentToken}"`;
 };
