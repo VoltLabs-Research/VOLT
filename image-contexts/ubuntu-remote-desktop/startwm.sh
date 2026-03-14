373893e97ed166d6b76884e3d7ccab1e253929a3
rodyherrera <contact@rodyherrera.com>
2026-03-14 20:46:21 -0300

    fix(container-xrdp): ship managed XRDP image with explicit XFCE startup

---
+++ image-contexts/ubuntu-remote-desktop/startwm.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eu
+
+if [ -r /etc/profile ]; then
+    . /etc/profile
+fi
+
+if [ -r "$HOME/.profile" ]; then
+    . "$HOME/.profile"
+fi
+
+export DESKTOP_SESSION=xfce
+export XDG_CURRENT_DESKTOP=XFCE
+export XDG_SESSION_DESKTOP=xfce
+export XDG_SESSION_TYPE=x11
+
+if command -v dbus-launch >/dev/null 2>&1; then
+    exec dbus-launch --exit-with-session startxfce4
+fi
+
+exec startxfce4
